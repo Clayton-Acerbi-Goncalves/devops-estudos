0fc800317ebc506d01eac4f86aec7fda1dc6d415
Clayton Acerbi Goncalves <clayton.acerbi1975@gmail.com>
2026-05-12 08:13:34 +0100

    feat: update app message to test CI/CD pipeline

--- docker/app.py
+++ docker/app.py
@@ -4,6 +4,6 @@ class Handler(BaseHTTPRequestHandler):
    def do_GET(self):
       self.send_response(200)
       self.end_headers()
-      self.wfile.write(b"Hello from Clayton's Docker container!")
+      self.wfile.write(b"Hello from Clayton's Docker container - CI/CD working!")
 
 HTTPServer(('0.0.0.0', 8080), Handler).serve_forever()
